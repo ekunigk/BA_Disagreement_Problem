cbea68ffd1d2bd6616869fb9e4f8c211d0bfbaad
ellenkunigk <s6elkuni@uni-bonn.de>
2024-06-10 16:18:59 +0200

    umap legend + changes

--- evaluation/umap.py
+++ evaluation/umap.py
@@ -4,14 +4,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import umap
 
-
-def visualize_umap(dataset, scale=False):
-    
+def project_umap(dataset, scale=False):
     feature_set = dataset[:, :-1]
     if scale:
         feature_set = StandardScaler().fit_transform(feature_set)
     reducer = umap.UMAP()
     embedding = reducer.fit_transform(feature_set)
+    return embedding
+
+def visualize_umap(dataset, embedding, scale=False):
 
     plt.scatter(
         embedding[:, 0],
@@ -20,6 +21,25 @@ def visualize_umap(dataset, scale=False):
     )
     plt.gca().set_aspect('equal', 'datalim')
     plt.title('UMAP projection of the dataset', fontsize=24)
+
+    plt.show()
     return embedding
 
+
+def visualize_umap2(dataset, embedding, scale=False):
+
+    label = dataset[:, -1].numpy().astype(int)
+    
+    
+
+    sca = sns.scatterplot(
+        y=embedding[:, 0],
+        x=embedding[:, 1],
+        hue=dataset[:, -1].numpy().astype(int),
+        palette='deep'
+    )
+    sca.legend(loc='upper right')
+    sca.set()
+    plt.show()
+    return embedding
 
