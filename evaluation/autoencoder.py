cf0c7d0b2f9be9174d019ab1613d26c729e0a860
ellenkunigk <s6elkuni@uni-bonn.de>
2024-07-17 15:45:55 +0200

    rankings

--- evaluation/autoencoder.py
+++ evaluation/autoencoder.py
@@ -9,9 +9,9 @@ class Encoder(nn.Module):
     def __init__(self, input_dim, hidden_dim):
         super(Encoder, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Linear(input_dim, 8),
+            nn.Linear(input_dim, 16),
             nn.Tanh(),
-            nn.Linear(8, hidden_dim),
+            nn.Linear(16, hidden_dim),
             nn.Tanh()
         )
 
@@ -24,9 +24,9 @@ class Decoder(nn.Module):
     def __init__(self, hidden_dim, output_dim):
         super(Decoder, self).__init__()
         self.decoder = nn.Sequential(
-            nn.Linear(hidden_dim, 8),
+            nn.Linear(hidden_dim, 16),
             nn.Tanh(),
-            nn.Linear(8, output_dim),
+            nn.Linear(16, output_dim),
             nn.Tanh()
         )
 
